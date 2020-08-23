ba0b4321537cd32ce41b9aa4bc5fd0dad7891ec1
Khalil Satyadama <khalilandaal@gmail.com>
2020-08-23 19:45:43 +0700

    Added sprite control based on mouse position

--- Assets/2-Gerakan-dan-Animasi/Scripts/MovementInput.cs
+++ Assets/2-Gerakan-dan-Animasi/Scripts/MovementInput.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class MovementInput : MonoBehaviour
 {
+  private int directionID;
   private Animator animator;
+  private Vector3 mouseScreenToWorld, mouseToPlayer;
   public float speed = 5f;
 
   void Start()
@@ -19,12 +21,15 @@ public class MovementInput : MonoBehaviour
     
     //Input axis are normalized to ensure every possible movements are in the same speed
     Vector3 movement = new Vector3(inputX, inputY, 0f).normalized;
+    mouseScreenToWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    mouseToPlayer = (mouseScreenToWorld - transform.position);
+    directionID = (int)((((Mathf.Atan2(mouseToPlayer.y, mouseToPlayer.x) * Mathf.Rad2Deg)+360f)%360 + 22.5f)/ 45)%8;
+    Debug.Log((Mathf.Atan2(mouseToPlayer.y, mouseToPlayer.x) * Mathf.Rad2Deg + 360f)%360 + ", "+directionID + "("+ mouseToPlayer.x + ", "+mouseToPlayer.y+")");
+    Debug.DrawLine(transform.position, mouseScreenToWorld, Color.red);
+    // animator.SetFloat("moveHori", movement.x);
+    // animator.SetFloat("moveVert", movement.y);
+    animator.SetInteger("directionIndex", directionID);
 
     transform.Translate(movement * speed * Time.deltaTime);
-
-    if (animator != null) {
-      animator.SetBool("idle", movement.magnitude <= 0f);
-      animator.SetFloat("move_x", movement.x);
-    }
   }
 }
